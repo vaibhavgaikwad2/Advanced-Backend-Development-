ebf6b863678a4a701a62cd820e0e577c8c92539c
Vaibhav Gaikwad <vaibhavgaikwad7007@gmail.com>
2025-07-02 22:43:35 +0530

    update

--- playground.js
+++ playground.js
@@ -37,4 +37,77 @@ function ternary(){
     console.log(result)
 }
 
-ternary();
+function if_elsee(){
+    var age=20;
+    if(age>=18){
+        console.log("Adult");
+
+    }
+    else{
+        console.log("Minor");
+    }
+}
+
+function else_if(){
+    if(10<5){
+        console.log("False");
+
+    }else if(10>5 ){
+        console.log("True");
+    }
+}
+
+
+
+function SwitchCases(){
+    let day = "Monday";
+
+    switch(day){
+        case "Monday":
+            console.log("Working day");
+            break;
+
+        case "Sunday":
+            console.log("Rest Day");
+
+            break;
+
+
+        
+    }
+}
+
+function evenodd(){
+    let x = 27;
+    if(x%2==0){
+        console.log("Number is Even");
+    }
+    else{
+        console.log("Number is odd");
+    }
+}
+
+function greaternum(){
+    let x =34;
+    let y=27;
+    let z = 54;
+
+    if(x>y){
+        if(x>z){
+            console.log("X is greater num");
+        }
+        else{
+            console.log("Z is greater num");
+        }
+
+    }else{
+        if(y>z){
+            console.log("y is greater num");
+
+        }
+        else{
+            console.log("z is greater num");
+        }
+    }
+}
+greaternum();
