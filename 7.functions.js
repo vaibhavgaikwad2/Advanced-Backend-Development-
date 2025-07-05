8b4b8ba657e78deb279668148b6ee1ef9ce035d1
Vaibhav Gaikwad <vaibhavgaikwad7007@gmail.com>
2025-07-05 11:24:12 +0530

    update

---
+++ 7.functions.js
@@ -0,0 +1,17 @@
+ function evenodd(num){
+
+    if(num%2==0){
+        console.log("Even number");
+    }
+    else{
+        console.log("odd");
+    }
+
+    
+ }
+
+ evenodd(5);
+
+ for(let i=1; i<=20; i++){
+    evenodd(i);
+ }
